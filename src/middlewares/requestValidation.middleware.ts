9ae80ddec6f82dea45698da4d439c382ee7456ed
Thyago Euclides <flooddk@gmail.com>
2025-07-20 23:22:35 -0400

    feat: implement book management features with validation and error handling

---
+++ src/middlewares/requestValidation.middleware.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import { ZodObject, ZodRawShape, ZodTypeAny } from "zod";
+
+interface IRequestSchemas {
+  params?: ZodTypeAny;
+  body?: ZodTypeAny;
+  query?: ZodTypeAny;
+}
+
+export class RequestValidation {
+  static execute(schemas: IRequestSchemas) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        if (schemas.params) {
+          (req as any).params = await schemas.params.parseAsync(req.params);
+        }
+
+        if (schemas.body) {
+          (req as any).body = await schemas.body.parseAsync(req.body);
+        }
+
+        if (schemas.query) {
+          (req as any).query = await schemas.query.parseAsync(req.query);
+        }
+
+        next();
+      } catch (error) {
+        next(error); 
+      }
+    };
+  }
+}
